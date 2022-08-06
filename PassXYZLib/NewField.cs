90d08e74ee2001dc617ba97209536d1a7a1681a1
Roger Ye <shugaoye@yahoo.com>
2022-08-06 17:44:04 +0800

    Added IKeyValue, NewField and NewItem

---
+++ PassXYZLib/NewField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassXYZLib
+{
+    public class NewField : Field
+    {
+        public NewField(string key = "", string value="", bool isProtected=false) : base(key, value, isProtected)
+        {
+        }
+    }
+}
